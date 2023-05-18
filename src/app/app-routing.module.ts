5b1b5e88367b79de80175c29129a6d2abbba58b8
randall.erasmus <randall.erasmus@interfront.co.za>
2023-05-19 00:37:45 +0200

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 import {HomeComponent} from "./home/home.component";
 import {ContactUsComponent} from "./contact-us/contact-us.component";
-import {AboutUsComponent} from "./about-us/about-us.component";
 import {ProductsComponent} from "./products/products.component";
 import {TodayDealsComponent} from "./today-deals/today-deals.component";
 import {NewArrivalsComponent} from "./new-arrivals/new-arrivals.component";
@@ -16,7 +15,6 @@ const routes: Routes = [
   { path: 'todaydeals', component: TodayDealsComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'contact-us', component: ContactUsComponent },
-  { path: 'about-us', component: AboutUsComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -38,7 +37,6 @@ import {FlexLayoutModule} from "@angular/flex-layout";
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent,
     ContactUsComponent,
     HeaderComponent,
     FooterComponent,
