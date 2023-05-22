94fdcf72c4b677a2d6f4843e4b929ba4d0b8b883
randall.erasmus <randall.erasmus@interfront.co.za>
2023-05-22 22:41:43 +0200

    WIP for Screens

---
+++ src/app/account-header/account-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-header',
+  templateUrl: './account-header.component.html',
+  styleUrls: ['./account-header.component.css']
+})
+export class AccountHeaderComponent {
+
+}
