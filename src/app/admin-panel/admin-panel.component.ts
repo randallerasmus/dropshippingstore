daff8251082dd66b98cf006d4b95f42a9c05e7c2
randall.erasmus <randall.erasmus@interfront.co.za>
2023-06-15 23:56:05 +0200

    Admin Panel

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,8 +1,10 @@
-import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {SelectionModel} from "@angular/cdk/collections";
 import {NavItem} from "./model/NavItem";
 import {MediaMatcher} from "@angular/cdk/layout";
+import {MatSidenav} from "@angular/material/sidenav";
+import {Router} from "@angular/router";
 
 export interface PeriodicElement {
   name: string;
@@ -29,96 +31,29 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./admin-panel.component.css']
 })
 
-export class AdminPanelComponent implements OnDestroy {
+export class AdminPanelComponent implements OnInit {
 
-  // url --> https://stackblitz.com/edit/angular-side-nav-dynamic-expansive-menu?file=app%2Fsidenav-responsive-example.ts
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  user: any;
+  loggedIn = false;
 
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
+  constructor(private router: Router,
+  ) {
   }
 
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
+  ngOnInit() {
+    this.router.navigate(['/platform/profile']);
+
   }
 
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+  toggleSidenav() {
+    if (this.sidenav) {
+      this.sidenav.toggle();
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
   }
 
-  menu: NavItem [] = [
-    {
-      displayName: 'Escritorio',
-      iconName: 'desktop_windows',
-      route: 'escritorio',
-    },
-    {
-      displayName: 'Entradas GADE',
-      iconName: 'ballot',
-      route: 'entradasGADE',
-    },
-    {
-      displayName: 'Expedientes',
-      iconName: 'description',
-      children: [
-        {
-          displayName: 'Mis Expedientes',
-          iconName: 'how_to_reg',
-          route: '/misexpedientes'
-        },
-        {
-          displayName: 'Todos',
-          iconName: 'waves',
-          route: '/todos'
-        }
-      ]
-    },
-    {
-      displayName: 'Perfiles',
-      iconName: 'group',
-      children: [
-        {
-          displayName: 'Búsqueda Perfil',
-          iconName: 'search',
-          route: '/busquedaperfiles'
-        }
-      ]
-    }
-  ];
-  mobileQuery: MediaQueryList;
-
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
-
-  fillerContent = Array.from({length: 50}, () =>
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
-
-  private _mobileQueryListener: () => void;
-
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+  logout() {
+    this.router.navigate(['/login']);
   }
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
-
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
