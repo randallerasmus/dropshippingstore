71f5678f2242d3031553d8c2c52f7ea1ded7d438
randall.erasmus <randall.erasmus@interfront.co.za>
2023-06-20 01:03:22 +0200

    Admin Panel - components

---
+++ src/app/admin-panel/components/security-permissions/security-permissions.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SecurityPermissionsComponent } from './security-permissions.component';
+
+describe('SecurityPermissionsComponent', () => {
+  let component: SecurityPermissionsComponent;
+  let fixture: ComponentFixture<SecurityPermissionsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SecurityPermissionsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SecurityPermissionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/admin-panel/components/security-permissions/security-permissions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-permissions',
+  templateUrl: './security-permissions.component.html',
+  styleUrls: ['./security-permissions.component.css']
+})
+export class SecurityPermissionsComponent {
+
+}
